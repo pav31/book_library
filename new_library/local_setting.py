5b31420c7cde52117777f91363e45106a6a658b5
Pavel <database@razoom.org>
2015-01-23 20:46:53 +0300

    commented out local settings

--- new_library/local_setting.py
+++ new_library/local_setting.py
@@ -1,19 +1,19 @@
 from settings import PROJECT_ROOT, SITE_ROOT
 import os
 
-DEBUG = True
+# DEBUG = True
 
-TEMPLATE_DEBUG = True
+# TEMPLATE_DEBUG = True
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "libdb",
-        "USER": "pav31",
-        "PASSWORD": "123qweasd",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "libdb",
+#         "USER": "pav31",
+#         "PASSWORD": "123qweasd",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
